e129e0011525ca859fc1ace1ba9eb5005ea7a304
nn-andrew <nguyen.andrew.quang@gmail.com>
2020-07-07 22:10:43 -0700

    Initial Commit

---
+++ Gif MakerTests/Gif_MakerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Gif_MakerTests.swift
+//  Gif MakerTests
+//
+//  Created by Andrew Nguyen on 7/7/20.
+//  Copyright © 2020 six. All rights reserved.
+//
+
+import XCTest
+@testable import Gif_Maker
+
+class Gif_MakerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
